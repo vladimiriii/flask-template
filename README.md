bb7841ad184db0b00455b896f0a453e8706a3bab
vladimiriii <brett.romero@hotmail.com>
2018-11-17 18:10:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Webapp Template
+Template for creating webapps
+
+#### Install
+bash install.sh
+
+#### Run
+bash run.sh
---
+++ app/static/script/functions.js
@@ -0,0 +1,4 @@
+// Functions to be used in the application
+function myFunction(){
+	console.log("My function is working");
+};
