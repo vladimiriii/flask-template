fb165e9564163e134b71e1a2e83165def3cfd92e
vladimiriii <brett.romero@hotmail.com>
2018-11-17 18:19:40 +0100

    Adding example AJAX function.

--- app/static/script/functions.js
+++ app/static/script/functions.js
@@ -2,3 +2,20 @@
 function myFunction(){
 	console.log("My function is working");
 };
+
+function exampleAjax(data, url) {
+
+	$.ajax({
+	  	type: "POST",
+	  	url: url,
+	  	data: data,
+		dataType: "text",
+		success: function(result) {
+			// Parse json string
+			let json = JSON.parse(result);
+		},
+		error: function(msg){
+			$(postTo).append(msg);
+		}
+	});
+};
